915ef97674384d59365a4d35647ed2e7617c2d1a
Bastien <bastien.patureau@yahoo.com>
2022-06-15 10:48:25 +0200

    winning screen

--- main.js
+++ main.js
@@ -5,32 +5,104 @@
 //détecter si il y a un alignement de 3 (recherche necessaire)
 //si oui, victoire pour le symbole qui à l'alignement
 const $board = document.querySelector(".board")
-let whosTurn = ""
+const $winningMessage = document.querySelector(".winning-message")
+const $winningMessageTxt = document.querySelector("#winningMessageText")
+let turnCount
+let whosTurn
+let victoire
 const init = () => {
   $board.addEventListener("click", e => handleClick(e))
   gameStart();
 }
 
 const handleClick = (e) => {
+  if(e.target.classList.contains("x") || e.target.classList.contains("circle")){
+    return
+  }
   e.target.classList.add(whosTurn)
   $board.classList.remove(whosTurn)
+
+  checkVictory()
+
   if(whosTurn === "x") {
     whosTurn = "circle"
-    console.log(whosTurn)
   }else {
     whosTurn = "x"
-    console.log(whosTurn)
   }
+  console.log(victoire)
+  if(turnCount >= 9 && !victoire) {
+    gameOver()
+    return
+  }
+  turnCount++
   $board.classList.add(whosTurn)
 }
 
+const checkVictory = () => {
+  //victoire horizontale haute
+  if($board.childNodes[1].classList.contains(whosTurn) && $board.childNodes[3].classList.contains(whosTurn) && $board.childNodes[5].classList.contains(whosTurn)) {
+    victoire = true
+    gameOver()
+    return
+  }
+    //victoire horizontale centrale
+  if($board.childNodes[7].classList.contains(whosTurn) && $board.childNodes[9].classList.contains(whosTurn) && $board.childNodes[11].classList.contains(whosTurn)) {
+    victoire = true
+    gameOver()
+    return
+  }
+    //victoire horizontale basse
+  if($board.childNodes[13].classList.contains(whosTurn) && $board.childNodes[15].classList.contains(whosTurn) && $board.childNodes[17].classList.contains(whosTurn)) {
+    victoire = true
+    gameOver()
+    return
+  }
+  //victoire verticale gauche
+  if($board.childNodes[1].classList.contains(whosTurn) && $board.childNodes[7].classList.contains(whosTurn) && $board.childNodes[13].classList.contains(whosTurn)) {
+    victoire = true
+    gameOver()
+    return
+  }
+    //victoire verticale centrale
+  if($board.childNodes[3].classList.contains(whosTurn) && $board.childNodes[9].classList.contains(whosTurn) && $board.childNodes[15].classList.contains(whosTurn)) {
+    victoire = true
+    gameOver()
+    return
+  }
+    //victoire verticale droite
+  if($board.childNodes[5].classList.contains(whosTurn) && $board.childNodes[11].classList.contains(whosTurn) && $board.childNodes[17].classList.contains(whosTurn)) {
+    victoire = true
+    gameOver()
+    return
+  }
+  //victoire diagonale gauche-droite
+  if($board.childNodes[1].classList.contains(whosTurn) && $board.childNodes[9].classList.contains(whosTurn) && $board.childNodes[17].classList.contains(whosTurn)) {
+    victoire = true
+    gameOver()
+    return
+  }
+  //victoire diagonale droite-gauche
+  if($board.childNodes[5].classList.contains(whosTurn) && $board.childNodes[9].classList.contains(whosTurn) && $board.childNodes[13].classList.contains(whosTurn)) {
+    victoire = true
+    gameOver()
+    return
+  }
+}
+
 const gameStart = () => {
+  victoire = false
+  turnCount="1"
   whosTurn = "x"
   $board.classList.add(whosTurn)
 }
 
-const gameWon = () => {
-
+const gameOver = () => {
+  $winningMessage.classList.add("show")
+  if(turnCount >= 9 && !victoire) {
+    $winningMessageTxt.innerHTML = `Match nul`
+    return 
+  }
+  $winningMessageTxt.innerHTML = `${whosTurn} a gagné !`   
 }
 
 init()
